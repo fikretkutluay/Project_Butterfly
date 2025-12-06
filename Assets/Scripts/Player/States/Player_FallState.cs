19b5852196e26cf0fb3541220ebdb3d6c09babde
Burak Enes Portakal <burak.enesportakal1@gmail.com>
2025-12-06 16:00:38 +0300

    çatıya karakter attım

---
+++ Assets/Scripts/Player/States/Player_FallState.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Player_FallState : Player_AirState
+{
+    public Player_FallState(Player player, StateMachine stateMachine, string animBoolName) : base(player, stateMachine, animBoolName)
+    {
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        if (player.groundDetected)
+            stateMachine.ChangeState(player.idleState);
+    }
+}
